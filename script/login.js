0e4a2cb2ab8e6edd9c6c3ab052219fbd6ab5ea9f
kawser0x <kawserswe@gmail.com>
2026-02-22 01:59:26 +0600

    Log in function work properly

--- script/login.js
+++ script/login.js
@@ -1,1 +1,20 @@
-console.log('angta lege gese')
+
+document.getElementById("btn-login").addEventListener("click",function(){
+    // console.log("Log in button clicked");
+    const inputNumber = document.getElementById('input-number');
+    const contacNumber = inputNumber.value ;
+    // console.log(contacNumber);
+    const inputPin = document.getElementById('input-pin');
+    const pin = inputPin.value ;
+    // console.log(pin)
+    if(contacNumber == '01234567890' && pin == '1234'){
+        // console.log('login successful');
+        alert("Login Successfull");
+        // window.location.replace("./home.html")
+        window.location.assign("./home.html");
+    }
+    else{
+        alert("Login Failed");
+        return ;
+    }
+})
