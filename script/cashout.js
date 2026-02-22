82aa5fa6671332434b040563548de590a8c13925
kawser0x <kawserswe@gmail.com>
2026-02-23 00:36:35 +0600

    cashout system implemented primarily

--- script/cashout.js
+++ script/cashout.js
@@ -1,3 +1,37 @@
 document.getElementById("btn-cashout").addEventListener("click",function(){
-    console.log("HEllo ")
-})
+    // console.log("HEllo ")
+    const cashoutNumberInput = document.getElementById('cashout-number');
+    const cashoutNumber = cashoutNumberInput.value ;
+    console.log(cashoutNumber);
+    if(cashoutNumber.length != 11){
+        alert("Invalid Agent Number");
+        return ;
+    }
+    const cashoutAmountInput = document.getElementById('cashout-amount');
+
+    const cashoutAmount = cashoutAmountInput.value ;
+    // console.log(cashoutAmount);
+
+    const balanceAmount = document.getElementById('balance');
+    const balance = balanceAmount.innerText ;
+
+    // console.log(balance);
+    const newBalance = Number(balance) - Number(cashoutAmount);
+    
+    if(newBalance < 0){
+        alert("Insufficient Amount");
+        return ;
+    }
+
+    const cashoutPinInput = document.getElementById('cashout-pin');
+    const pin = cashoutPinInput.value ;
+    if(pin === '1234'){
+        alert("cashout successfull");
+        // console.log('new balance : ',newBalance);
+        balanceAmount.innerText = newBalance;
+    }else{
+        alert('invalid pin');
+        return ;
+    }
+
+});
